597f356eb9d2749ac5f0a8f1bb26d016b4a9313e
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-07-25 18:00:51 -0400

    Update root component with Signup and Login buttons, and functionality for their dialogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+// App's root component file (default entry point to the app). When user launches app, root component is displayed as home page.
+
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RouterOutlet } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+
+  // This function will open the dialog when the signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px' // Assign a width to the dialog
+    });
+  }
+
+  // This function will open the dialog when the login button is clicked
+  openLoginDialog(): void {
+    this.dialog.open(LoginFormComponent, {
+      width: '280px' // Assign a width to the dialog
+    });
+  }
 }
