a40d9ca106c418ebf012fb4d84e4641879fd488d
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-09-17 17:11:20 -0400

    Display user info in Profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-import { FetchApiDataService } from '../fetch-api-data.service';
+import { Component, OnInit } from '@angular/core';
+// import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: any = {}; // Store user info, including favorite movies
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getUserInfo();
+  }
+
+  // Fetch user info from the API
+  getUserInfo(): void {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      this.user = JSON.parse(storedUser);
+      console.log('User data from localStorage:', this.user);
+    } else {
+      console.error('No user data found in localStorage.');
+    }
+  }
 }
