5c6d18ad20eb1240a6d3fbdefbf9a37530d1a0d8
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-09-17 15:38:31 -0400

    Create UserProfileComponent

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.scss'
+})
+export class UserProfileComponent {
+}
