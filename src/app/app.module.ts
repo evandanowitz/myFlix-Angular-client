1ca3fa2290defbbfa118613875007d78310f8dc6
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-09-17 15:40:19 -0400

    Import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,59 @@
-import { NgModule } from '@angular/core';
+// // This is the entry point of your app, meaning that it's mostly used to wire up different modules together and express dependencies.
+
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-import { MatInputModule } from '@angular/material/input';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { FormsModule } from '@angular/forms';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+const appRoutes: Routes = [
+  // A welcome route that'll point to a WelcomePageComponent that will act as a welcome page with sign-up and login options for users.
+  { path: 'welcome', component: WelcomePageComponent },
+  // A route that'll point to a MovieCardComponent that will display movies for users.
+  { path: 'movies', component: MovieCardComponent },
+  // An empty route, which will route to the WelcomePageComponent by default.
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginFormComponent,
+    MovieCardComponent,
     UserRegistrationFormComponent,
-    LoginFormComponent
+    WelcomePageComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    MatInputModule,
+    BrowserModule,
+    FormsModule,
     MatButtonModule,
     MatCardModule,
-    MatFormFieldModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
     MatSnackBarModule,
-    FormsModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     provideClientHydration(),
