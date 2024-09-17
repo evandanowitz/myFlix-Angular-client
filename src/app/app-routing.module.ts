a6e9ab4253746951b3dfd43c1c3b9c224c156187
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-09-17 15:40:04 -0400

    Implement routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome-page', component: WelcomePageComponent },
+  { path: 'movie-card', component: MovieCardComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'login-form', component: LoginFormComponent },
+  { path: '', redirectTo: 'welcome-page', pathMatch: 'full' }, // Default route,
+  { path: '**', redirectTo: '/movie-card' } // Fallback route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
