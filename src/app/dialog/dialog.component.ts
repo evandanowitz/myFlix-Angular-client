c751356cda6da7dd1e3e0cbd6d41d38de1a603bd
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-09-20 14:50:03 -0400

    Create dialogs for genre, director, synopsis to display movie info upon click

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,13 @@
+// The DialogComponent will be responsible for displaying the info passed to it (e.g., genre details, director bio, or synopsis).
+
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.scss'
+})
+export class DialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { } // MAT_DIALOG_DATA token is used to pass data (genre, director, synopsis) to the dialog
+}
